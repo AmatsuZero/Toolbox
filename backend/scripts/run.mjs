11f866066ef9f3a55aed3fe630ed8a130c7ba418
AmatsuZero <jzh16s@hotmail.com>
2023-04-04 19:07:20 +0800

    FIX: windows 脚本无法启动

--- backend/scripts/run.mjs
+++ backend/scripts/run.mjs
@@ -15,12 +15,28 @@ if (!fs.existsSync(ventPath)) {
 
 process.env["venv_dir"] = ventPath;
 process.env["LAUNCH_SCRIPT"] = path.join(stableFusionDir, "launch.py");
-process.env["python3"] = path.join(ventPath, "bin", "python3");
 process.env["COMMANDLINE_ARGS"] = "--skip-torch-cuda-test --precision full --no-half"; // 跳过 cuda
 
-spawn("sh", [path.join(stableFusionDir, "webui.sh")], {
-    shell: true,
-    stdio: "inherit"
-});
+if (process.platform === "win32") {
+    const bat = spawn("cmd.exe", [path.join(stableFusionDir, "webui.bat")]);
+
+    bat.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    bat.stderr.on('data', (data) => {
+        console.error(data.toString());
+    });
+
+    bat.on('exit', (code) => {
+        console.log(`Child exited with code ${code}`);
+    });
+} else {
+    spawn("sh", [path.join(stableFusionDir, "webui.sh")], {
+        shell: true,
+        stdio: "inherit"
+    });
+}
+
 
 
